1a9fe614400a6eda07312f8fca890f53aba36f00
JR Strayhorn <jrstrayhorn@gmail.com>
2023-12-01 17:31:32 -0500

    done with day 1 part 1, part 2 is wip

--- puzzles/day-1/day-1-a.ts
+++ puzzles/day-1/day-1-a.ts
@@ -3,7 +3,18 @@ import chalk from 'chalk';
 
 export async function day1a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  let total = 0;
+  data.forEach((d) => {
+    if (!d) return;
+    // replacing letters w/ blanks
+    const numArray = d.replace(/[a-z]/g, '').split('');
+    const firstNum = numArray[0];
+    // going -2 b/c of /r that is at the end of the strings...
+    const lastNum = numArray[numArray.length - 2];
+    // console.log(firstNum + lastNum);
+    total += +`${firstNum + lastNum}`;
+  });
+  return total;
 }
 
 const answer = await day1a();
--- puzzles/day-1/day-1-b.ts
+++ puzzles/day-1/day-1-b.ts
@@ -1,9 +1,46 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+export const NUMBER_TO_NUMBER_MAP = {
+  one: '1',
+  two: '2',
+  three: '3',
+  four: '4',
+  five: '5',
+  six: '6',
+  seven: '7',
+  eight: '8',
+  nine: '9',
+};
+
 export async function day1b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  const regExpLetterCheck = /[a-z]/g;
+  const regExpNumberStringCheck =
+    /(?:one|two|three|four|five|six|seven|eight|nine)/g;
+  let total = 0;
+  data.forEach((d, idx) => {
+    if (!d) return;
+    // replacing number string w/ numbers, letters w/ blanks
+    const numArray = d
+      // replacing number strings 'two' w/ numbers '2'
+      .replace(
+        regExpNumberStringCheck,
+        (match) => NUMBER_TO_NUMBER_MAP[match] ?? ''
+      )
+      // replacing remaining letters w/ blanks
+      .replace(regExpLetterCheck, '')
+      // get array of number
+      .split('');
+    const firstNum = numArray[0];
+    // going -2 b/c of /r that is at the end of the strings...
+    const lastNum = numArray[numArray.length - 2];
+    // console.log(numArray, firstNum + lastNum);
+    // coerce string to number and add to total
+    console.log(idx, numArray);
+    total += +`${firstNum + lastNum}`;
+  });
+  return total;
 }
 
 const answer = await day1b();
